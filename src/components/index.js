7473f3d91d8973308075adb4ba585ed9a72d9133
karuna14 <kkaushik@bt.iitr.ac.in>
2020-07-07 20:38:57 +0530

    add minimax, multiplayer, vs computer game

--- src/components/index.js
+++ src/components/index.js
@@ -1,175 +1,99 @@
-import React, { Component } from 'react'
-import './src/css/main.css'
-import Result from './Result'
-import NewGame from './ResetButton'
-import Tile from './sketch';
+import React, { Component } from 'react';
+import { AppContext } from './AppProvider';
+import { TYPES_OF_GAME,  PLAYER_TURNS, ICONS_CHAR} from './minimax';
 import Robot from './src/img/robot.svg'
 import Human from './src/img/human.svg'
+import './src/css/main.css'
+import Board from './sketch';
 import Hints from './hints';
-export default class MainPage extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-          gameBoard: [
-              ' ',' ',' ',
-              ' ',' ',' ',
-              ' ',' ',' '
-          ],
-          winner: null,
-          turn: 'x',
-          count: 0
-        }
-      }
-
-      resetBoard(){
-        this.setState({
-          gameBoard: [
-            ' ',' ',' ',
-            ' ',' ',' ',
-            ' ',' ',' '   
-          ],
-          winner: null,
-          turn: 'x',
-          count: 0
-        });
-      }
-
-      // tie(board) {
-      //   var moves = board.join('').replace(/ /g, '');
-      //   if (moves.length === 9) {
-      //     return true;
-      //   }
-      //   return false;
-      // }
 
 
-      updateBoard(loc, player) {
-      console.log(this.state.winner, this.state.turn, this.state.gameBoard);
-      if(this.state.winner !== null) {
-        console.log("Winner", this.state.winner);
-        return;
-      }
-      if(this.state.gameBoard[loc]=== 'x' || this.state.gameBoard[loc]=== 'o'){
-        return;
-      }
-      let currentGameBoard = this.state.gameBoard;
-      currentGameBoard.splice(loc, 1, this.state.turn);
-      this.setState({gameBoard: currentGameBoard}, function(){
-        var moves = this.state.gameBoard.join('').replace(/ /g,'');
-        console.log('Moves:', moves, 'Winner:', this.state.winner);
-        if(moves.length === 9) {
-          this.setState({winner:"Noone"});
-          return;
+const GameType = (props) => {
+    const { value, name } = props;
+
+    return (
+      <AppContext.Consumer>
+        {context => (
+          <li 
+            onClick={() => context.changeType(value)} 
+            className={value === context.gameType ? "active" : ""}> 
+            {name} 
+          </li>
+        )}
+      </AppContext.Consumer>
+    )
+  }
+
+class MainPage extends Component {
+  render() {
+
+    let textInfo = '';
+    const currentIconType = this.context.currentIcon;
+
+    if (this.context.gameState.isTie) {
+      textInfo = 'Tie!';
+    } else {
+      if (this.context.gameType === TYPES_OF_GAME.HUMAN_PLAYERS) {
+        if (this.context.gameState.position === "") {
+          textInfo = `It's player(${ICONS_CHAR[currentIconType]}) turn`;
         } else {
-          var topRow = this.state.gameBoard[0] + this.state.gameBoard[1] + this.state.gameBoard[2];
-          if(topRow.match(/xxx|ooo/)){
-            this.setState({winner: this.state.turn});
-            return;
-            }
-          var middleRow = this.state.gameBoard[3] + this.state.gameBoard[4] + this.state.gameBoard[5];
-          if(middleRow.match(/xxx|ooo/)){
-            this.setState({winner: this.state.turn});
-            return;
-          }
-          var bottomRow = this.state.gameBoard[6] + this.state.gameBoard[7] + this.state.gameBoard[8];
-          if(bottomRow.match(/xxx|ooo/)){
-            this.setState({winner: this.state.turn});
-            return;
-          }
-          var leftCol = this.state.gameBoard[0] + this.state.gameBoard[3] + this.state.gameBoard[6];
-          if(leftCol.match(/xxx|ooo/)){
-            this.setState({winner: this.state.turn});
-            return;
-          }
-          var middleCol = this.state.gameBoard[1] + this.state.gameBoard[4] + this.state.gameBoard[7];
-          if(middleCol.match(/xxx|ooo/)){
-            this.setState({winner: this.state.turn});
-            return;
-          }
-          var rightCol = this.state.gameBoard[2] + this.state.gameBoard[5] + this.state.gameBoard[8];
-          if(rightCol.match(/xxx|ooo/)){
-            this.setState({winner: this.state.turn});
-            return;
-          }
-          var leftDiag = this.state.gameBoard[0] + this.state.gameBoard[4] + this.state.gameBoard[8];
-          if(leftDiag.match(/xxx|ooo/)){
-            this.setState({winner: this.state.turn});
-            return;
-          }
-          var rightDiag = this.state.gameBoard[2] + this.state.gameBoard[4] + this.state.gameBoard[6];
-          if(rightDiag.match(/xxx|ooo/)){
-            this.setState({winner: this.state.turn});
-            return;
-          }
-          this.setState({turn: (this.state.turn === 'x') ? 'o' : 'x' });
+          textInfo = `Player(${ICONS_CHAR[1 - currentIconType]}) wins!`;
         }
-      }, this);
-       
+      } else {
+        if (this.context.gameState.position === "") {
+          if (this.context.playerTurn === PLAYER_TURNS.HUMAN) textInfo = `It's your turn`;
+          else textInfo = `It's computer turn`;
+        } else {
+          if (this.context.playerTurn === PLAYER_TURNS.HUMAN) textInfo = `Computer win!`;
+          else textInfo = `You win!`;
+        }
       }
-
-      componentDidMount() {
-        setInterval(() => {
-          return this.setState(( state, props) => {
-            return{
-              count : state.count == 15 ? 0 :state.count+1
-            }
-          })
-          
-        }, 1000);
-      }
-
-    render() {
-
-        return(
-            <div className="container">
-                <div className="instructionDiv">
-                    <div>
-                    <NewGame reset={this.resetBoard.bind(this)} />
-                    </div>
-                    <h1> Hints/instruction</h1>
-                    <div className="hintsDiv">
-                      <Hints />
-                    </div>
-
-                </div>
-
-                <div className="gameDiv">
-                <div className="timerDiv">
-                    Time: {this.state.count}  Seconds &nbsp;&nbsp;
-                    <button className="restartButton" onClick={this.resetBoard.bind(this)}>Restart</button>
-                  </div>
-                <div className="resultShow">
-                    <Result winner={this.state.winner} />
-                    </div>
-                     <br />
-                    <h1> Tic Tac Toe</h1>
-                    <div className="gameBox">
-                    {this.state.gameBoard.map(function(value, i){
-                        return(
-                        <Tile 
-                            key={i}
-                            loc={i}
-                            value={value}
-                            updateBoard={this.updateBoard.bind(this)}
-                            turn={this.state.turn} />
-                        );
-                        }.bind(this))}
-                    </div>
-                </div>
-
-                <div className="playresouterDiv">
-                    <div className="players">
-                    <div className="robotDiv">
-                        <img src={Robot} /><p>Computer/AI Or Another Player  </p>
-                    </div>
-                    <h1>Vs</h1>
-                    <div className="humanDiv"> 
-                        <img src={Human} /><p >Player / Human</p>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        )
     }
+    return (
+      <div className="container">
+      <div className="headerDiv">
+        <h1>Tic Tac Toe</h1>
+
+         <div className="playersDiv">
+                
+                <div className="playersBox">
+                     <Hints />
+                    <div className="playerinnerBox">
+                        <div className="humanplayers">
+                            <img src={Human} alt="Human" />
+                            <GameType value={TYPES_OF_GAME.HUMAN_PLAYERS} name="2 Human-Players" />
+                        </div>
+
+                        <div  className="computerVS">
+                            <img src={Robot} alt="ROBOT" />
+                            <GameType value={TYPES_OF_GAME.VS_COMPUTER} name="Versus Computer/AI" />
+                        </div>
+                    </div>
+                  
+             </div>
+            
+                <div className="gameBox">
+              <div className="newgameButton">
+                  <button onClick={() => this.context.newGame()}>New Game</button>
+              </div>
+              <div className="info">{textInfo}</div>
+                <Board />
+              </div>
+          </div>
+
+       </div>
+      </div>
+
+      
+    );
+  }
 }
+
+MainPage.contextType = AppContext;
+
+export default MainPage;
+
+
+
+
+
